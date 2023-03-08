0c29619953d5ee9192296b189c012ce9fefb6f93
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-03-08 11:26:19 -0500

    created ExerciseVideos Component using Youtube Search and Download API(RapidApi)

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,29 +1,38 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
-import { exerciseOptions, fetchData } from "../utils/fetchData";
+import { exerciseOptions, fetchData, youtubeOptions } from "../utils/fetchData";
 import Detail from "../components/Detail";
 import ExerciseVideos from "../components/ExerciseVideos";
 import SimilarExercises from "../components/SimilarExercises";
 
 function ExerciseDetail() {
   const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([]);
   const { id } = useParams();
-  console.log(id);
+  // console.log(id);
 
   useEffect(() => {
     const fetchExercisesData = async () => {
       const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
       const youtubeSearchUrl =
-        "https://youtube-media-downloader.p.rapidapi.com";
+        "https://youtube-search-and-download.p.rapidapi.com";
 
       // fetch exercise data by its id.
       const exerciseDetailData = await fetchData(
         `${exerciseDbUrl}/exercises/exercise/${id}`,
         exerciseOptions
       );
-      console.log(exerciseDetailData);
+      // console.log(exerciseDetailData);
       setExerciseDetail(exerciseDetailData);
+
+      //fetch similar YouTube videos
+      const exerciseVideosData = await fetchData(
+        `${youtubeSearchUrl}/search?query=${exerciseDetailData.name} exercise`,
+        youtubeOptions
+      );
+      console.log(exerciseVideosData);
+      setExerciseVideos(exerciseVideosData.contents);
     };
     fetchExercisesData();
   }, [id]);
@@ -31,7 +40,10 @@ function ExerciseDetail() {
   return (
     <Box>
       <Detail exerciseDetail={exerciseDetail} />
-      <ExerciseVideos />
+      <ExerciseVideos
+        exerciseVideos={exerciseVideos}
+        name={exerciseDetail.name}
+      />
       <SimilarExercises />
     </Box>
   );
