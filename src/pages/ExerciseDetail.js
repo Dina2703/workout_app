89b7264d98cbaa0e04211535108f6e35a94bbe18
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-03-08 09:51:46 -0500

    Exercises Detail, fetch exercioses detail by its id, and pass it down to the Detail component.

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,7 +1,39 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+import Detail from "../components/Detail";
+import ExerciseVideos from "../components/ExerciseVideos";
+import SimilarExercises from "../components/SimilarExercises";
 
 function ExerciseDetail() {
-  return <div>ExerciseDetail</div>;
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
+  console.log(id);
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+      const youtubeSearchUrl =
+        "https://youtube-media-downloader.p.rapidapi.com";
+
+      // fetch exercise data by its id.
+      const exerciseDetailData = await fetchData(
+        `${exerciseDbUrl}/exercises/${id}`,
+        exerciseOptions
+      );
+      setExerciseDetail(exerciseDetailData);
+    };
+    fetchExercisesData();
+  }, [id]);
+
+  return (
+    <Box>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
+  );
 }
 
 export default ExerciseDetail;
